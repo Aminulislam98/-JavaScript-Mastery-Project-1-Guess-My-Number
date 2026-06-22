34286612012b8687c0dee9a35fd0ec1a089947de
Aminul islam <aminurrohman98@gmail.com>
2026-06-22 11:40:52 +0100

    practiced about javascript dom

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 'use strict';
 // PROJECT #1 Guess my number
-console.log(document.querySelector('.message').textContent);
-
-//  I learned what is dom. dom stands for document object model, mean, dom is a web api(a programming interface) provided by browser, mean the browser does all heavy lifting.
+// console.log(document.querySelector('.message').textContent);
+// selecting and manipulating element
+// document.querySelector('.message').textContent = 'Correct Number 🎉';
+// document.querySelector('.guess').value;
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+  !guess
+    ? (document.querySelector('.alert').textContent =
+        `Please guess number between 1 - 20`)
+    : (document.querySelector('.alert').textContent = '');
+});
