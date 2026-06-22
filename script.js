5a6a9f190fa15496122755ff85f8a65388b171fd
Aminul islam <aminurrohman98@gmail.com>
2026-06-22 11:01:32 +0100

    I learned what is dom. dom stands for document object model, mean, dom is a web api(a programming interface) provided by browser, mean the browser does all heavy lifting.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 'use strict';
 // PROJECT #1 Guess my number
 console.log(document.querySelector('.message').textContent);
+
+//  I learned what is dom. dom stands for document object model, mean, dom is a web api(a programming interface) provided by browser, mean the browser does all heavy lifting.
